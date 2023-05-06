53b1ed4460e91b75e42d1f9eddc1a97992cc6d28
El-Behery <dev.elbehery@gmail.com>
2023-05-06 22:14:30 +0300

    responsive changes, sweet alert, form handling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+//! Regex Variables
+const nameReg = /\w+/;
+const messageReg = /\w+/;
+const emailReg = /^[\w-\.]+@([\w-]+\.)+[\w-]+$/;
+
+//! Variables
+const contactForm = document.querySelector(".contact-us form");
+const formName = document.querySelector("input[name='gameName']");
+const formEmail = document.querySelector("input[name='userEmail']");
+const formSelect = document.querySelector("select[name='formSelect']");
+const selectDefaultValue = formSelect.value;
+const formMessage = document.querySelector("textarea[name='formMessage']");
+
 //! Header Toggler for Small Screens
 const header = document.querySelector("nav");
 const icon = document.querySelector("#header-toggler .fa-solid");
@@ -15,8 +28,59 @@ const handleToggleClick = () => {
   }
 };
 
+const sendQuery = (query) => {
+  emailjs
+    .send("service_mgmott9", "template_3eqkyzr", query)
+    .then(() => console.log("Email was sent Successfully"));
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const query = {
+    from_name: formName.value,
+    email_id: formEmail.value,
+    game_category: formSelect.value,
+    message: formMessage.value,
+  };
+
+  if (!nameReg.test(formName.value)) {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Please Enter Your Name",
+    });
+  } else if (!emailReg.test(formEmail.value)) {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Please Enter Your Email",
+    });
+  } else if (formSelect.value === selectDefaultValue) {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Please Select Your Option",
+    });
+  } else if (!messageReg.test(formMessage.value)) {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Please Enter Your Message",
+    });
+  } else {
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Your Request has been sent successfully.",
+    });
+    sendQuery(query);
+  }
+};
+
 //! Event Listeners
 headerToggler.addEventListener("click", handleToggleClick);
+contactForm.addEventListener("submit", handleSubmit);
+window.addEventListener("load", () => this.scrollTo(0, 0));
 
 //! Particles Plugin
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
